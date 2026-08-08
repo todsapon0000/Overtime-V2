b813c8dfb984838af62d3f790d99f3aca98eeb1a
todsapon0000 <todsapon0000@gmail.com>
2026-08-09 01:28:55 +0700

    style(ui): set otDataTable zoom scale to 90% for better responsiveness

--- js/app.js
+++ js/app.js
@@ -319,7 +319,7 @@ async function renderOTTable(selectedMonth) {
             }
         </style>
         <div class="table-responsive">
-            <table id="otDataTable" class="table table-hover table-striped align-middle text-center mb-0 w-100 border custom-border-table">
+            <table id="otDataTable" class="table table-hover table-striped align-middle text-center mb-0 w-100 border custom-border-table" style="zoom:90%">
                 <thead class="table-light text-muted small align-middle">
                     <tr>
                         <th rowspan="2" class="text-start align-middle" style="width: 12%;">
